f183486e3265748d8faefda5e0906bee6d05614b
Alex Kehl <alexander.kehl@apprologic.de>
2020-12-09 01:57:06 +0100

    Make blog work with SSR

---
+++ components/LoginForm.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { useForm } from 'react-hook-form'
+import { performLogin } from '../logic/auth'
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
+
+export default function SignIn() {
+  const classes = useStyles()
+
+  const { register, errors, handleSubmit } = useForm()
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit(performLogin())}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            label="Email Address"
+            helperText={errors.email && 'Invalid email Format'}
+            error={Boolean(errors.email)}
+            autoComplete="email"
+            autoFocus
+            name="email"
+            inputRef={register({
+              required: true,
+              pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, // eslint-disable-line
+            })}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            inputRef={register({
+              minLength: 3,
+              required: true,
+            })}
+            name="password"
+            label="Password"
+            error={Boolean(errors.password)}
+            helperText={
+              errors.password && 'Password length must be greater than 8'
+            }
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
+  )
+}
---
+++ pages/login.tsx
@@ -0,0 +1,9 @@
+import LoginFormComponent from '../components/LoginForm'
+
+export const LoginForm = (): JSX.Element => (
+  <div className="container">
+    <LoginFormComponent />
+  </div>
+)
+
+export default LoginForm
