e52208a6a0526820d8ca4436e94238cc13888a17
Alex Kehl <alexander.kehl@apprologic.de>
2021-01-15 05:26:23 +0100

    Remove category image maping from component

---
+++ pages/gallery/CategoryPictures.js
@@ -0,0 +1,144 @@
+export default {
+  Acryl: [
+    {
+      src: '/Acryl/Tchernikov Anatoly 50x40 acrylic, baumvolle..jpeg',
+      width: 40,
+      height: 50,
+    },
+    {
+      src:
+        '/Acryl/Tchernikov Anatoly. 50x50. Утренний Амлет. acrylic, baumvolle..jpeg',
+      width: 50,
+      height: 50,
+    },
+    {
+      src:
+        '/Acryl/Tchernikov Anatoly 50 х 40.acrylic, baumvolle . Недопонимание..jpeg',
+      width: 50,
+      height: 40,
+    },
+    {
+      src:
+        '/Acryl/Tchernikov Anatoly. 50х80. acrylic, baumvolle. Одуванчики..jpeg',
+      width: 1200,
+      height: 518,
+    },
+    {
+      src:
+        '/Acryl/Tchernikov Anatoly. 70x200. Strikayel. acrylic, baumvolle.jpeg',
+      width: 200,
+      height: 70,
+    },
+    {
+      src: '/Acryl/Tchernikov Anatoly 70x50 acrylic, baumvolle.-Schvelbe.jpeg',
+      width: 50,
+      height: 70,
+    },
+    {
+      src:
+        '/Acryl/Tchernikov Anatoly. Завтрок на обочине. 47 x 83cm. acrylic, baumvolle..jpeg',
+      width: 83,
+      height: 47,
+    },
+    {
+      src:
+        '/Acryl/Tchernikow Anatoly. 50x50. Вьезд в Иерусалим с восточных варот. acrylic, baumvolle..jpeg',
+      width: 50,
+      height: 50,
+    },
+    {
+      src:
+        '/Acryl/Tchernikow Anatoly 70x50 acrylic, baumvolle Гранатовый Рассвет.jpeg',
+      width: 1280,
+      height: 885,
+    },
+    {
+      src:
+        '/Acryl/Tchernikow Anatoly 70x50 acrylic, baumvolle из цыкла Друзья Человека 1.jpeg',
+      width: 50,
+      height: 70,
+    },
+    {
+      src:
+        '/Acryl/Tchernikow Anatoly 70x50 acrylic, baumvolle из цыкла Друзья Человека 4..jpeg',
+      width: 50,
+      height: 70,
+    },
+    {
+      src:
+        '/Acryl/Tchernikow Anatoly 70x50 acrylic, baumvolle из цыкла Друзья Человека 5.jpeg',
+      width: 50,
+      height: 70,
+    },
+    {
+      src:
+        '/Acryl/Tschernikov Anatoly. 50 x 70 cm. acrul, baumwolle. Царант стабильности..jpeg',
+      width: 70,
+      height: 50,
+    },
+    {
+      src: '/Acryl/Tschernikov Anatoly. baumwolle.acryl. 65x47cm..jpeg',
+      width: 47,
+      height: 65,
+    },
+    {
+      src: '/Acryl/Tschernikov Anatoly. Бальшой Фиш. parzelan.acryl..jpeg',
+      width: 1280,
+      height: 1255,
+    },
+    {
+      src:
+        '/Acryl/Tschernikov Anatoly.  Ключ верности. argalit. acryl. 50 x 70cm..jpeg',
+      width: 70,
+      height: 50,
+    },
+    {
+      src:
+        '/Acryl/Tschernikov Anatoly. Лето Юнности. baumwolle.acryl. 40 x 50..jpeg',
+      width: 50,
+      height: 40,
+    },
+    {
+      src:
+        '/Acryl/Tschernikov Anatoly. .Ожидание прихода Месяца. baumwolle.acryl. 50 х 80cm.jpeg',
+      width: 80,
+      height: 50,
+    },
+    {
+      src:
+        '/Acryl/Tschernikov Anatoly. Рекламный щит ф.ALG. 95x49 акрил, арголит.jpeg',
+      width: 40,
+      height: 95,
+    },
+    {
+      src: '/Acryl/Tschernikow Anatoly.Весна пришла. Дратути. .50x40akri.jpg',
+      width: 40,
+      height: 50,
+    },
+  ],
+  Oil: [
+    {
+      src:
+        '/Oil/Tschernikov Anatoly 25 x 30 Oil, baumvolle. Утренняя россыпь.jpeg',
+      width: 30,
+      height: 25,
+    },
+    {
+      src:
+        '/Oil/Tschernikov Anatoly . «Белянки»-III.30х25.бязь,масло. 25 x 30 Oil, baumvolle..jpeg',
+      width: 30,
+      height: 25,
+    },
+    {
+      src: '/Oil/В горах. 60 х 80. холст,масло.jpeg',
+      width: 80,
+      height: 60,
+    },
+    {
+      src:
+        '/Oil/Черников Анатолий Николаевич. "Яблоки на Aarstrasse".26,7x35.масло,аргалит.jpeg',
+      width: 35,
+      height: 27,
+    },
+  ],
+}
