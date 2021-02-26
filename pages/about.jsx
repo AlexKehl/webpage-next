efe7d0eddb02cd00c7cbb4add73ef1be42b8bbf5
Alex Kehl <alexander.kehl@apprologic.de>
2021-02-26 01:23:39 +0100

    Add user login logic

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,13 +1,10 @@
 import About from '../components/About'
 import WithHeader from '../components/WithHeader'
-import WithAuth from '../components/WithAuth'
 
 export const AboutPage = () => (
   <div className="container">
     <About />
   </div>
 )
-
-export const getServerSideProps = WithAuth
 
 export default WithHeader(AboutPage)
--- pages/api/sessions.js
+++ pages/api/sessions.js
@@ -1,28 +1,34 @@
-import { withIronSession } from 'next-iron-session'
+import withSession from '../../lib/session'
 
 const VALID_EMAIL = 'test@test.com'
 const VALID_PASSWORD = '123'
 
-const handler = async (req, res) => {
-  if (req.method !== 'POST') {
-    return res.status(404).send('')
+export default withSession(async (req, res) => {
+  console.log(req.method)
+  if (req.method === 'GET') {
+    const user = req.session.get('user')
+
+    if (user) {
+      return res.json({
+        isLoggedIn: true,
+        ...user,
+      })
+    } else {
+      return res.json({
+        isLoggedIn: false,
+      })
+    }
   }
 
-  const { email, password } = req.body
+  if (req.method === 'POST') {
+    const { email, password } = req.body
 
-  if (email === VALID_EMAIL && password === VALID_PASSWORD) {
-    req.session.set('user', { email })
-    await req.session.save()
-    return res.status(201).send({ isLoggedIn: true })
+    if (email === VALID_EMAIL && password === VALID_PASSWORD) {
+      req.session.set('user', { email })
+      await req.session.save()
+      return res.status(201).send({ isLoggedIn: true })
+    }
   }
 
   return res.status(403).send('')
-}
-
-export default withIronSession(handler, {
-  cookieName: process.env.SESSION_COOKIE_NAME,
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production' ? true : false,
-  },
-  password: process.env.APPLICATION_SECRET,
 })
