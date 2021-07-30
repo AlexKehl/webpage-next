4064a339eba7fbeaf2fe516df514cda27748a2c6
Alex Kehl <alexander.kehl@apprologic.de>
2021-07-30 13:32:17 +0200

    Add css for dropzone

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import '../src/globalstyles/gallery.css'
+import 'react-dropzone-uploader/dist/styles.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
