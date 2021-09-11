0b2c6b1d07ed391a2cf88a4b9046abc427754ab4
Alex Kehl <alexander.kehl@apprologic.de>
2021-09-11 13:31:53 +0200

    Add next.config.js again (for deployment?)

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  i18n: {
+    locales: ['en', 'de', 'ru'],
+    defaultLocale: 'en',
+  },
+  env: {
+    BASE_URL: 'http://localhost:3000',
+    API: 'https://anatolykehl.ru/api',
+    ENV: 'production',
+    PUBLIC_API_MOCKING_ENABLED: false,
+  },
+}
