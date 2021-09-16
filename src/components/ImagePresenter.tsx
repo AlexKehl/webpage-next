31d4639eba611c7ce6e27eab2f862b10633932d6
Alex Kehl <alexander.kehl@apprologic.de>
2021-09-16 12:00:26 +0200

    Gallery adjustments

--- src/components/ImagePresenter.tsx
+++ src/components/ImagePresenter.tsx
@@ -1,6 +1,7 @@
 import { InfoIcon } from '@chakra-ui/icons'
 import React from 'react'
-import Image from 'next/image'
+import { Box, Flex } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 
 interface Props {
   onClick?: (event: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
@@ -10,25 +11,18 @@ interface Props {
 
 const ImagePresenter = ({ onClick, src, onInfoClick }: Props) => {
   return (
-    <span className="inline-block relative mx-3 mb-3 p-0 w-56 h-56 bg-gray-100 rounded-lg">
-      {onInfoClick && (
-        <InfoIcon
-          boxSize="1.5em"
-          border="black"
-          color="white"
-          className="absolute mt-2 ml-48"
-          onClick={onInfoClick}
-        />
-      )}
-
-      <Image
-        layout="fill"
-        objectFit="contain"
-        onClick={onClick}
-        src={src}
-        alt=""
+    <Flex p="1" mx="2" width="40" bgColor="gray.200">
+      <InfoIcon
+        m="1"
+        position="absolute"
+        cursor="pointer"
+        boxSize="1.5em"
+        color="white"
+        onClick={onInfoClick}
+        _hover={{ color: 'gray.200' }}
       />
-    </span>
+      <Image src={src} fit="contain" alt="Segun Adebayo" cursor="pointer" />
+    </Flex>
   )
 }
 
