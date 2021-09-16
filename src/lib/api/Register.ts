842e57a52db3c79bca8d8c2429f6ea822372156e
Alex Kehl <alexander.kehl@apprologic.de>
2021-09-16 16:51:30 +0200

    Refactor

--- src/lib/api/Register.ts
+++ src/lib/api/Register.ts
@@ -1,22 +1,11 @@
 import axios from 'axios'
 import { Endpoints } from '../../../common/constants/Endpoints'
-import { ConfirmEmailDto, RegisterDto } from '../../../common/interface/Dto'
+import { ConfirmEmailDto } from '../../../common/interface/Dto'
 import { API } from '../../constants/EnvProxy'
-
-const registerRequest = async (registerDto: RegisterDto): Promise<any> => {
-  const { data } = await axios.post<any>(
-    `${API}${Endpoints.register}`,
-    registerDto,
-    {
-      withCredentials: true,
-    }
-  )
-  return data
-}
 
 const confirmEmailTokenRequest = async (dto: ConfirmEmailDto) => {
   const { data } = await axios.post(`${API}${Endpoints.emailConfirm}`, dto)
   return data
 }
 
-export { registerRequest, confirmEmailTokenRequest }
+export { confirmEmailTokenRequest }
--- src/lib/hooks/useRegister.ts
+++ src/lib/hooks/useRegister.ts
@@ -1,9 +1,10 @@
 import { useDisclosure } from '@chakra-ui/react'
 import { useForm } from 'react-hook-form'
+import { Endpoints } from '../../../common/constants/Endpoints'
 import HttpStatus from '../../../common/constants/HttpStatus'
 import { RegisterDto } from '../../../common/interface/Dto'
-import { registerRequest } from '../api/Register'
-import { handleHttpError } from '../errors/Handlers'
+import { API } from '../../constants/EnvProxy'
+import { postWithErrHandle } from '../api/Utils'
 import useI18n from './useI18n'
 import useToasts from './useToasts'
 
@@ -23,17 +24,19 @@ const useRegister = ({ onClose }: Options) => {
   const { showError } = useToasts()
 
   const onSubmit = async (registerDto: RegisterDto) => {
-    try {
-      await registerRequest(registerDto)
-      onClose()
-      onConfirmEmailOpen()
-    } catch (error) {
-      handleHttpError({
-        error,
-        default: () => showError({ text: t.unexpectedError }),
-        [HttpStatus.CONFLICT]: () => showError({ text: t.emailAlreadyTaken }),
-      })
-    }
+    postWithErrHandle({
+      params: {
+        url: `${API}${Endpoints.register}`,
+        data: registerDto,
+        credentials: 'include',
+      },
+      onSuccess: () => {
+        onClose()
+        onConfirmEmailOpen()
+      },
+      [HttpStatus.CONFLICT]: () => showError({ text: t.emailAlreadyTaken }),
+      default: () => showError({ text: t.unexpectedError }),
+    })
   }
 
   const arePasswordsMatching = (passwordRepeat: string) => {
