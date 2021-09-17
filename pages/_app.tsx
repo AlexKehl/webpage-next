1bc3e566f49e08bd5894ebe70df9e5f8c1baf93e
Alex Kehl <alexander.kehl@apprologic.de>
2021-09-17 14:06:03 +0200

    Move _app to tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { startMock } from '../src/mocks'
 import { ENV, PUBLIC_API_MOCKING_ENABLED } from '../src/constants/EnvProxy'
 import Layout from '../src/components/Layout'
+import { FullPageLoaderContextProvider } from '../src/lib/contexts/FullPageLoaderContext'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: any) {
   if (ENV === 'development' && PUBLIC_API_MOCKING_ENABLED) {
     startMock()
   }
   return (
     <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <FullPageLoaderContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </FullPageLoaderContextProvider>
     </ChakraProvider>
   )
 }
